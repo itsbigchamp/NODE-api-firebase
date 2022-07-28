4c7b9e55eda357d18c09e6411886e31e80431aea
Gavin Anderson <itsbigchamp@Gavins-MacBook-Air.local>
2022-07-28 16:28:40 -0400

    added delete feature

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,33 @@
 const functions = require("firebase-functions");
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 //setup routes...
-app.get('/test', (req, res) => {
-    res.send('wow it actually Worked.')
+app.get("/test", (req, res) => {
+  res.send("wow it actually Worked.");
 });
 
-app.get('/another-one', (req, res) => {
-    res.send('this one Works, too!')
+app.get("/another-one", (req, res) => {
+  res.send("this one Works, too!");
+});
+
+app.post('/add-one', (req, res)=> {
+    res.send('add this one as well')
+});
+
+app.delete('/delete-one', (req, res) => {
+    Array.splice(3,1)
 })
 
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
 
 exports.helloThere = functions.https.onRequest((req, res) => {
-    res.send('Hello there Firebase Cloud User')
+  res.send("Hello there Firebase Cloud User");
 });
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
